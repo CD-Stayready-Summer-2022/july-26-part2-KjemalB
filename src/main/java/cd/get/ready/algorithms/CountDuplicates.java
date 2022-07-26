b186510d7acda627684d8131abddde7fca5076f5
Khalil Brewington <kbrewing02@gmail.com>
2022-07-26 10:55:03 -0400

    first method

--- src/main/java/cd/get/ready/algorithms/CountDuplicates.java
+++ src/main/java/cd/get/ready/algorithms/CountDuplicates.java
@@ -1,5 +1,7 @@
 package cd.get.ready.algorithms;
 
+import java.util.HashMap;
+
 public class CountDuplicates {
     /**
      * Example of overloading a function that accepts a Array String
@@ -7,7 +9,17 @@ public class CountDuplicates {
      * @return
      */
     public Integer countDuplicates(String[] input){
-        return null;
+        HashMap<String, Integer> wordCount = new HashMap<String, Integer>();
+        int count = 0;
+        for (String word : input) {
+            if (!wordCount.containsKey(word)) {
+                wordCount.put(word, 0);
+            } else {
+                wordCount.put(word, wordCount.get(word) + 1);
+                count++;
+            }
+        }
+        return count;
     }
 
     /**
@@ -16,6 +28,15 @@ public class CountDuplicates {
      * @return
      */
     public Integer countDuplicates(Integer[] input){
-        return null;
+        HashMap<Integer, Integer> numCount = new HashMap<>();
+        int count = 0;
+
+        for (int num : input) {
+            if(numCount.containsKey(num)) {
+                count++;
+            } else
+                numCount.put(num, 1);
+        }
+        return count;
     }
 }
